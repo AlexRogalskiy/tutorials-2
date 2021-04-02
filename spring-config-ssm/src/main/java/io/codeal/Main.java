ef13e5c6995128d30484194d315bddfd38572ff4
Jackson Coelho <fcjack@gmail.com>
2021-04-02 15:59:39 +0200

    Remove unnecessary annotation

--- spring-config-ssm/src/main/java/io/codeal/Main.java
+++ spring-config-ssm/src/main/java/io/codeal/Main.java
@@ -3,11 +3,9 @@ package io.codeal;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class Main implements CommandLineRunner {
 
     @Value("${db.username}")
